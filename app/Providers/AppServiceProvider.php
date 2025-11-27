7fda74bca1cdeb34c8830b9549eceb172e428c5d
Максим Похильчук <pohilchuk.m@roistat.com>
2025-11-27 12:45:53 +0300

    add events

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,9 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Event::listen(
-            TaskCreated::class,
-            SendTaskCreatedNotification::class
-        );
+
     }
 }
